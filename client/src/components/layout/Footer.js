a76a2a2cb7bc6757626fac810ab7df2893d67921
ThomasJd <thomasdinh25@gmail.com>
2018-12-14 23:49:58 -0600

    profiles component

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -7,3 +7,13 @@ export default () => {
     </footer>
   );
 };
+import React from "react";
+
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} ThomasjD
+    </footer>
+  );
+};
